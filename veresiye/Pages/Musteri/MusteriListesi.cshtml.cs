c6a5b611a93f3b139da6386fa5babf69628d6912
makbas <06br9385@gmail.com>
2023-02-24 00:59:28 +0300

    partial view donusum ve musteri listesi print mode

---
+++ veresiye/Pages/Musteri/MusteriListesi.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace veresiye.Pages.Musteri
+{
+    public class MusteriListesiModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ veresiye/Pages/Musteri/_MusteriArama.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace veresiye.Pages.Musteri
+{
+    public class _MusteriAramaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
