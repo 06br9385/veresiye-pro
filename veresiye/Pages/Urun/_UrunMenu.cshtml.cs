78cc49d5b02dd8049781587f27e25a60e50f259e
makbas <06br9385@gmail.com>
2023-02-24 22:21:43 +0300

    urun-kartı tasarım

---
+++ veresiye/Pages/Urun/_UrunMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace veresiye.Pages.Urun
+{
+    public class _UrunMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
